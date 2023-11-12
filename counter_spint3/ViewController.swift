1dc258ea642959baa86488c14dfb2e26c5d506c3
Bugakov Sergey <bugakov-ss@yandex.ru>
2023-11-12 13:22:49 +0300

    Выполнено задание для 3 спринта: 1) Добавлена кнопка, которая добавляет +1 после нажатия на кнопку 2) Добавлен Lable, которыый заполняется при загрузке и отображение увеличения числа при нажатие на кнопку

--- counter_spint3/ViewController.swift
+++ counter_spint3/ViewController.swift
@@ -9,10 +9,22 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var countLabel: UILabel!
+    @IBOutlet weak var plusCountButton: UIButton!
+    
+    private var countSomething: Int = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        countLabel.text = "0"
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func plusTouchButton(_ sender: Any) {
+        countSomething += 1
+        countLabel.text = "\(countSomething)"
+    }
+    
 
 
 }
